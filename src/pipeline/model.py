c45da00746b04667c3cc5ecbb23a8e992db37661
pareeknikhil <nikhilpareek149@gmail.com>
2025-09-06 10:27:30 -0400

    addition dvc version to mlflow

--- src/pipeline/model.py
+++ src/pipeline/model.py
@@ -2,9 +2,12 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+import subprocess
+
 import mlflow
 import numpy as np
 import tensorflow as tf
+import yaml
 from mlflow.models import ModelSignature
 from mlflow.types.schema import Schema, TensorSpec
 from tensorflow.keras import (Input, Model, callbacks, layers, models,
@@ -24,6 +27,7 @@ ml_model_path = os.environ.get("ML_MODEL_PATH")
 mlflow_path = os.environ.get('MLRUNS_PATH')
 combined_emg_path = os.environ.get('COMBINED_EMG_FILE')
 depedency_path = os.environ.get('DEPENDENCY_FILE')
+dvc_path = os.environ.get('DVC_PATH')
 
 mlflow.set_tracking_uri(uri=mlflow_path)
 mlflow.set_experiment(experiment_name='gesture-classification')
@@ -40,6 +44,15 @@ def load_tfrecord(filename):
 def load_dataset(selected_type):
     return load_tfrecord(filename=f"{tfrecord_path}/{selected_type}/{combined_emg_path}")
 
+def read_dvc_version():
+    try:
+        with open(dvc_path, 'r') as f:
+            dvc_meta = yaml.safe_load(f)
+        dataset_version = dvc_meta['outs'][0]['md5']
+    except Exception as e:
+        print('Failed to read DVC File:', e)
+        dataset_version = 'XXX'
+    return dataset_version
 
 def run_model() -> None:
     with mlflow.start_run() as run:
@@ -90,7 +103,6 @@ def run_model() -> None:
 
         mlflow.log_artifacts(local_dir=log_path, artifact_path='tensorboard_logs')
 
-
         model_input = Schema(inputs=[TensorSpec(type=np.dtype(np.float32), shape= (-1, ML_WINDOW, num_emg_channels), name="EMG_Channels_1_to_8 (OpenBCI)")])
         model_output = Schema(inputs=[TensorSpec(type=np.dtype(np.float32), shape= (-1, get_num_labels()), name="Hand_Gestures (Classification)")])
 
@@ -99,26 +111,26 @@ def run_model() -> None:
         mlflow.keras.log_model(best_model, 'best_model', pip_requirements = depedency_path, signature=model_signature)
 
         params = {
-            'Batch Size' : BATCH_SIZE,
-            'Sequence length' : ML_WINDOW,
-            'Input Dimension' : num_emg_channels,
-            'Overlap Data-Points' : ML_WINDOW_OVERLAP,
-            'EPOCHS' : EPOCHS,
-            'Optimizer Name' : 'adam',
-            'Learning Rate' : LEARNING_RATE,
-            'Buffer Size' : BUFFER_SIZE,
+            'batch_size' : BATCH_SIZE,
+            'sequence_length' : ML_WINDOW,
+            'input_dimension' : num_emg_channels,
+            'overlap' : ML_WINDOW_OVERLAP,
+            'epochs' : EPOCHS,
+            'optimizer_name' : 'adam',
+            'learning_rate' : LEARNING_RATE,
+            'buffer_size' : BUFFER_SIZE,
             'ReduceOnPlateau' : 'monitor:val_loss, factor:0.5, patience:10, min_lr:0.00001, verbose:1',
-            'NumberOfGestures' : get_num_labels()
+            'number_of_classes' : get_num_labels(),
+            'dataset_version' : read_dvc_version()
         }
         
         mlflow.log_params(params=params)
 
         metrics = {
-            'Best Validation Accuracy' : max(history.history['val_accuracy']),
-            'Best Validation Loss' : min(history.history['val_loss']),
-            'Best Training Accuracy' : max(history.history['accuracy']),
-            'Best Training Loss' : min(history.history['loss'])
-
+            'best_val_acc' : max(history.history['val_accuracy']),
+            'best_val_loss' : min(history.history['val_loss']),
+            'best_train_acc' : max(history.history['accuracy']),
+            'best_train_loss' : min(history.history['loss'])
         }
 
         mlflow.log_metrics(metrics)
