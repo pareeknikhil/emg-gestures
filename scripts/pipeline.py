564fed3eb9e0f2efb25a0f3a8d9ab38ae5d442de
pareeknikhil <nikhilpareek149@gmail.com>
2025-09-23 23:48:58 -0400

    emg2vec embedding

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -8,9 +8,9 @@ load_env_variables()
 from src.pipeline.combineTFR import (combine_labels,
                                      print_combine_sample_collected)
 from src.pipeline.deleteTFR import delete_old_files
-from src.pipeline.embedding import run_embedding
 from src.pipeline.labelTFR import create_window, print_sample_collected
 from src.pipeline.model import run_model
+from src.pipeline.rae_embedding import run_embedding
 from src.pipeline.scale import print_scaled_sample_collected, scale
 from src.utils import \
     gpu_config  # # [TECH DEBT: add a method instead of script-run]
