7e042e72a97789ace6bd8fb46a5de16c4eb6b4b6
pareeknikhil <nikhilpareek149@gmail.com>
2025-09-29 16:29:34 -0400

    WORKING: emg latetent embedding: normal

--- src/pipeline/model.py
+++ src/pipeline/model.py
@@ -2,6 +2,9 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+import csv
+import json
+
 import matplotlib.pyplot as plt
 import mlflow
 import numpy as np
@@ -11,6 +14,7 @@ import tensorflow as tf
 import yaml
 from mlflow.models import ModelSignature
 from mlflow.types.schema import Schema, TensorSpec
+from tensorboard.plugins import projector
 from tensorflow.keras import (Input, Model, callbacks, layers, models,
                               optimizers)
 
@@ -31,6 +35,9 @@ combined_emg_path = os.environ.get('COMBINED_EMG_FILE')
 depedency_path = os.environ.get('DEPENDENCY_FILE')
 dvc_path = os.environ.get('DVC_PATH')
 artifact_path = os.environ.get('ARTIFACTS_PATH')
+label_idx_mapping_path = os.environ.get('LABEL_IDX_MAPPING')
+embedding_visualization_path = os.environ.get('EMBEDDING_TENSORBOARD')
+
 
 num_emg_channels = Source.get_num_emg_channels()
 
@@ -100,12 +107,14 @@ def run_model() -> None:
         embedding = layers.TimeDistributed(layers.Dense(units=170, activation='relu'))(normalize)
         lstm_one = layers.LSTM(400, return_sequences=True)(embedding)
         lstm_one = layers.Dropout(0.3)(lstm_one)
-        lstm_two = layers.LSTM(400)(lstm_one)
+        lstm_two = layers.LSTM(64, name="lstm_final")(lstm_one)
         lstm_two = layers.Dropout(0.3)(lstm_two)
         outputs = layers.Dense(units=get_num_labels(), activation='softmax')(lstm_two)
 
         model = Model(inputs, outputs)
         model.compile(optimizer=optimizers.Adam(learning_rate=LEARNING_RATE), loss='categorical_crossentropy', metrics=['accuracy'])
+
+        encoder_model = Model(inputs=model.input, outputs=model.get_layer('lstm_final').output)
 
         with open(f'{artifact_path}/model_summary.txt', 'w') as f:
             model.summary(print_fn=lambda x: f.write(x + '\n'))
@@ -116,7 +125,7 @@ def run_model() -> None:
 
         history = model.fit(train_ds, epochs=EPOCHS, steps_per_epoch=train_steps_per_epoch, validation_data=validation_ds, validation_steps=validation_steps_per_epoch, callbacks=callback_list)
 
-        mlflow.log_artifacts(local_dir=log_path, artifact_path='tensorboard_logs')
+        # mlflow.log_artifacts(local_dir=log_path, artifact_path='tensorboard_logs')
 
         model_input = Schema(inputs=[TensorSpec(type=np.dtype(np.float32), shape= (-1, TIMESTEPS, num_emg_channels), name="EMG_Channels_1_to_8 (OpenBCI)")])
         model_output = Schema(inputs=[TensorSpec(type=np.dtype(np.float32), shape= (-1, get_num_labels()), name="Hand_Gestures (Classification)")])
@@ -157,28 +166,91 @@ def run_model() -> None:
 
         mlflow.log_metrics(metrics)
 
-        test_ds = load_dataset(selected_type="test")
+        # test_pred, test_label = [], []
 
-        test_pred, test_label = [], []
+        # for feature, label in test_ds:
+        #     logits = best_model(feature[None, ...]) ## [TECH DEBT: could be optimized, currently one sample batch prediction]
+        #     test_pred.append(tf.argmax(input=logits, axis=-1))
+        #     test_label.append(tf.argmax(input=label, axis=-1))
 
-        for feature, label in test_ds:
-            logits = best_model(feature[None, ...]) ## [TECH DEBT: could be optimized, currently one sample batch prediction]
-            test_pred.append(tf.argmax(input=logits, axis=-1))
-            test_label.append(tf.argmax(input=label, axis=-1))
+        # label_classes = get_all_labels(selected_type='train')
 
-        label_classes = get_all_labels(selected_type='train')
+        # cm = tf.math.confusion_matrix(labels=test_label, predictions=test_pred).numpy()
 
-        cm = tf.math.confusion_matrix(labels=test_label, predictions=test_pred).numpy()
+        # df_cm = pd.DataFrame(data=cm,
+        #                     index=[f'Actual: {label}' for label in label_classes],
+        #                     columns=[f'Pred: {label}' for label in label_classes])
 
-        df_cm = pd.DataFrame(data=cm,
-                            index=[f'Actual: {label}' for label in label_classes],
-                            columns=[f'Pred: {label}' for label in label_classes])
+        # plt.figure(figsize=(10, 8))  # adjust size as needed
+        # sns.heatmap(df_cm, annot=True, fmt='d', cmap='Blues')
+        # plt.title('Confusion Matrix')
+        # plt.ylabel('Actual')
+        # plt.xlabel('Predicted')
+        # plt.tight_layout()
+        # plt.savefig(f'{artifact_path}/confusion_matrix.png')  # saves as PNG
+        # mlflow.log_artifact(f'{artifact_path}/confusion_matrix.png')
 
-        plt.figure(figsize=(10, 8))  # adjust size as needed
-        sns.heatmap(df_cm, annot=True, fmt='d', cmap='Blues')
-        plt.title('Confusion Matrix')
-        plt.ylabel('Actual')
-        plt.xlabel('Predicted')
-        plt.tight_layout()
-        plt.savefig(f'{artifact_path}/confusion_matrix.png')  # saves as PNG
-        mlflow.log_artifact(f'{artifact_path}/confusion_matrix.png')
+
+        with open(label_idx_mapping_path) as file:
+            label_idx_map = json.load(file)
+
+        keys = tf.constant([int(k) for k in label_idx_map.keys()], dtype=tf.int64)
+        values = tf.constant(list(label_idx_map.values()), dtype=tf.string)
+
+        table = tf.lookup.StaticHashTable(initializer=tf.lookup.KeyValueTensorInitializer(keys, values),
+                                          default_value="UNKNOWN")
+
+        def map_labels(window, label):
+            idx = tf.argmax(label, axis=-1)
+            gesture = table.lookup(idx)
+            latent_vector = encoder_model(window, training=False)
+            return latent_vector, gesture
+
+        mapped_ds = test_ds.map(map_labels)
+
+        test_labels = []
+        test_embeddings = []
+
+        for embedding, label in mapped_ds:
+            test_embeddings.append(embedding.numpy())
+            test_labels.extend([tf.compat.as_str_any(l.numpy()) for l in label])
+
+        test_embeddings = np.concatenate(test_embeddings, axis=0)
+
+        metadata_path = os.path.join(embedding_visualization_path, "metadata.tsv")
+
+        with open(metadata_path, "w") as f:
+            for label in test_labels:
+                f.write(f"{label}\n")
+
+        embedding_var = tf.Variable(test_embeddings, name="latent_embeddings")
+
+        checkpoint = tf.train.Checkpoint(embedding=embedding_var)
+        checkpoint.save(os.path.join(embedding_visualization_path, "embedding.ckpt"))
+
+        config = projector.ProjectorConfig()
+        embedding = config.embeddings.add()
+        embedding.tensor_name = "embedding/.ATTRIBUTES/VARIABLE_VALUE"
+        embedding.metadata_path = "metadata.tsv"  # relative path to log_dir
+        projector.visualize_embeddings(embedding_visualization_path, config)
+
+        mlflow.log_artifacts(local_dir=log_path, artifact_path='tensorboard_logs')
+
+
+        # test_embeddings: shape (N, D)
+        # test_labels:     length N
+
+        csv_path = f"{artifact_path}/embeddings_with_labels.csv"
+
+        with open(csv_path, "w", newline="") as f:
+            writer = csv.writer(f)
+            # header: first column is label, then embedding_0 ... embedding_{D-1}
+            header = ["label"] + [f"dim_{i}" for i in range(test_embeddings.shape[1])]
+            writer.writerow(header)
+
+            for label, emb in zip(test_labels, test_embeddings):
+                writer.writerow([label] + emb.tolist())
+
+        print(f"Saved {len(test_labels)} rows to {csv_path}")
+
+        mlflow.log_artifact(csv_path)
